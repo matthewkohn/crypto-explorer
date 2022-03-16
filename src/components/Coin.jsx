1f08d43cf4ce158fe79351d4d9bdf312c2d386f6
MattKohn <kohney@hotmail.com>
2022-03-16 13:47:29 -0600

    Build JSX for Coin component using formatted coin data for the selected coin

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,32 +1,43 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 function Coin({ formattedCoins }) {
   const param = useParams()
   const currentCoin = formattedCoins.filter((item) => item.id === param.id)[0]
+  const { rank, image, symbol, name, price, percentChange, sparkline, high24h, low24h, marketCap } = currentCoin;
+  
+  const chartStyles = {
+    background: "#00bdcc", 
+    height: "80px", 
+    width: "300px", 
+    borderRadius: 5, 
+    padding: 2
+  }
 
   return (
     <div>
-      <p>Coin!</p>
-      {console.log(currentCoin)}
+      <div style={{display: 'flex', padding: 40}}>
+        <img src={image} alt={name} style={{ width: 180, height: 180, marginRight: 80 }}/>
+        <div >
+          <p>{symbol}</p>
+          <h2>{name}</h2>
+          <h3>{price}</h3>
+          <Sparklines data={sparkline} style={chartStyles} margin={1} >
+            <SparklinesLine style={{ stroke: "white", fill: "none" }} />
+         </Sparklines>
+        </div>
+      </div>
+      <ul style={{ textAlign: 'center', listStyle: 'none', lineHeight: 1.7}}>
+        <li>Market Cap: {marketCap}</li>
+        <li>Market Cap Rank: #{rank}</li>
+        <li>24-hour High: {high24h}</li>
+        <li>24-hour Low: {low24h}</li>
+        <li>24-hour Change: {percentChange}</li>
+      </ul>
+      
     </div>
   )
 }
 
 export default Coin
-
-
-
-
-// import { Sparklines, SparklinesLine } from "react-sparklines";
-
-// const chartStyles = {
-//   background: "#00bdcc", 
-//   height: "80%", 
-//   width: "100%", 
-//   borderRadius: 5, 
-//   padding: 2
-// }
-/* <Sparklines data={params.value} style={chartStyles} margin={1}  >
-    //         <SparklinesLine style={{ stroke: "white", fill: "none" }} />
-    //       </Sparklines> */
--- src/functions/formatCoinData.jsx
+++ src/functions/formatCoinData.jsx
@@ -10,8 +10,8 @@ function formatCoin(coin) {
     price: formatPrice(coin.current_price),
     percentChange: `${coin.price_change_percentage_24h.toFixed(2)}%`,
     sparkline: [...coin.sparkline_in_7d.price],
-    high: formatPrice(coin.high_24h),
-    low: formatPrice(coin.low_24h),
+    high24h: formatPrice(coin.high_24h),
+    low24h: formatPrice(coin.low_24h),
     marketCap: `$${(coin.market_cap / 1000000).toFixed(3)} million`
   };
 }
