f74fe7f650064870bed851a9b041dda95acc6c0c
MattKohn <kohney@hotmail.com>
2022-03-27 20:55:28 -0600

    Adjust styling and functionality of Coin component

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,39 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { Paper, Grid, Typography, Button } from '@mui/material'
-// import { Paper, Grid, IconButton, Typography, Button } from '@mui/material'
-// import { StarOutline } from '@mui/icons-material'
 import { styled } from '@mui/material/styles'
 import { useNavigate, useParams } from 'react-router-dom'
-
-
-const Img = styled('img')({
-  margin: 'auto',
-  padding: '0 20px',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
+import { getCoinGeckoUrl, getDatabaseUrl } from '../util/urls';
 
 function Coin({ coinList, addCoin, likedCoins }) {
   const [description, setDescription] = useState('')
   const [isLiked, setIsLiked] = useState(false)
+
   const param = useParams()
   const navigate = useNavigate()
+
   const currentCoin = coinList.filter((item) => item.id === param.id)[0]
   const { id, rank, image, symbol, name, price, percentChange, high24h, low24h, marketCap } = currentCoin; 
 
   useEffect(() => {
-    const URL = `https://api.coingecko.com/api/v3/coins/${id}`
-    fetch(URL)
+    const coinGeckoUrl = getCoinGeckoUrl()
+    fetch(coinGeckoUrl + `/${id}`)
       .then((res) => res.json())
       .then((data) => setDescription(data.description.en))
-
-    const found = likedCoins.some(obj => obj.param === id)
+    // Check for duplication when CTA is clicked
+    const found = likedCoins.some(coin => coin.param === id)
     setIsLiked(found)
   }, [id, likedCoins])
 
   function handleAddCoin() {
-    fetch('http://localhost:4000/coins', {
+    const databaseUrl = getDatabaseUrl()
+    fetch(databaseUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -42,34 +35,29 @@ function Coin({ coinList, addCoin, likedCoins }) {
     })
       .then((res) => res.json())
       .then((data) => addCoin(data))
-      
-    navigate('/')
+      .then(navigate('/'))
   }
 
   return (
-    <Paper>
-      <Grid container spacing={20}>
-        <Grid item>
+    <GridPaper elevation={10} >
+      <GridBlock container spacing={8} >
+        <Grid item xs={6}>
           <Img src={image} alt={name} />
         </Grid>
-        <Grid item>
+        <Grid item xs={6}>
           <Typography variant='h2'>{name}</Typography>
           <Typography variant='h3'>{price}</Typography>
         </Grid>
-      </Grid>
-      <Grid container spacing={12} sx={{ padding: '40px' }}>
-        <Grid item>
-          <Button 
-            variant='outlined' 
-            size='large' 
-            // sx={{  padding: '30px', display: 'flex', alignItems: 'center' }}
-            onClick={isLiked ? () => navigate('/') : handleAddCoin}
-          >
-            {isLiked ? `${name} is awesome. Keep learning.` : 'Add to Portfolio'}
-          </Button>
-        </Grid>
-        <Grid item>
-          
+      </GridBlock>
+      <CallToAction 
+        variant='outlined' 
+        size='large' 
+        onClick={isLiked ? () => navigate('/') : handleAddCoin}
+      >
+        {isLiked ? `${name} is awesome. Keep learning.` : `Learn more about ${name}`}
+      </CallToAction>
+      <GridBlock container spacing={12} >
+        <Grid item xs={12} sm={6}>       
           <Typography variant='h6' gutterBottom >
             In the last 24 hours:
           </Typography>
@@ -82,17 +70,41 @@ function Coin({ coinList, addCoin, likedCoins }) {
             Change: {percentChange}
           </Typography>
         </Grid>
-        <Grid item>
-          <Typography variant="h6" gutterBottom >
+        <Grid item xs={12} sm={6}>
+          <Typography variant='h6' gutterBottom >
             Fun facts:
           </Typography>
           <Typography>Symbol: {symbol}</Typography>
           <Typography>Market Cap Rank: #{rank}</Typography>
           <Typography>Market Cap: {marketCap}</Typography>
         </Grid>
-      </Grid>
-    </Paper>
+      </GridBlock>
+    </GridPaper>
   )
 }
 
 export default Coin
+
+const Img = styled('img')({
+  margin: 'auto',
+  padding: '0 20px',
+  display: 'block',
+  maxWidth: '80%',
+  maxHeight: '80%',
+});
+
+const CallToAction = styled(Button)({
+  margin: '10px 0 0', 
+  padding: '10px 0', 
+  display: 'flex', 
+  alignItems: 'center',
+})
+
+const GridPaper = styled(Paper)({
+  display: 'flex', 
+  flexDirection: 'column',
+})
+
+const GridBlock = styled(Grid)({
+  padding: '30px',
+})
