2825acb50ecaa1b52238e4ac389ffebb7c14466c
MattKohn <kohney@hotmail.com>
2022-03-27 21:03:52 -0600

    Create styled components for LikedCoin component

--- src/components/LikedCoin.jsx
+++ src/components/LikedCoin.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Typography, Accordion, AccordionSummary, AccordionDetails, IconButton, Avatar } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { DeleteOutlined } from '@mui/icons-material'
+import { styled } from '@mui/material/styles'
 import { useNavigate } from 'react-router-dom'
 import parse from 'html-react-parser'
 
@@ -10,29 +11,43 @@ function LikedCoin({ coin, handleDelete }) {
   const navigate = useNavigate()
 
   return (
-    <Accordion elevation={5} sx={{ margin: '8px'}}>
+    <ChosenCoin elevation={5} >
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <Avatar 
           src={coin.image} 
           alt={coin.name} 
           onClick={() => navigate(`/coins/${coin.param}`)}
         />
-        <Typography sx={{ width: '70%', paddingLeft: '30px' }} variant='h6' >
+        <CoinName variant='h6' >
           {coin.name}
-        </Typography>
+        </CoinName>
         <IconButton onClick={() => handleDelete(coin.id)} >
           <DeleteOutlined />  
         </IconButton>        
       </AccordionSummary>
       <AccordionDetails >
-        <Typography 
-          sx={{ margin: 'auto', width: '70%', maxHeight: '200px', overflowY: 'scroll'}}
-        >
+        <Description>
           {parse(coin.description)}
-        </Typography>
+        </Description>
       </AccordionDetails>
-    </Accordion>
+    </ChosenCoin>
   )
 }
 
 export default LikedCoin
+
+const ChosenCoin = styled(Accordion)({
+  margin: '8px',
+})
+
+const CoinName = styled(Typography)({
+  width: '70%', 
+  paddingLeft: '50px',
+})
+
+const Description = styled(Typography)({
+  margin: 'auto', 
+  width: '70%', 
+  maxHeight: '200px', 
+  overflowY: 'scroll',
+})
